bad4816ae7ec1a76e7e9aa2fbafa9d7f12e15a3d
Jinanrachid <jinan.rachid@lau.edu>
2025-08-25 12:09:11 +0300

    This file contains the variables required by clean_data.py.

---
+++ variables.py
@@ -0,0 +1,186 @@
+footer = """Schedule a Free Consultation Today
+ Your
+ AI
+ & Cloud
+ Partner
+ Schedule Consultation
+ Solutions
+ Cloud Consulting & Migration
+ Cloud Modernization
+ Managed Services
+ Artificial Intelligence
+ Data & Analytics
+ Startup Accelerator
+ Company
+ Who We Are
+ Our Core Values
+ Our Team
+ Sustainability & CSR
+ News & Reads
+ News
+ Blogs
+ Case Studies
+ Other
+ Events
+ Privacy Policy
+ Terms & Conditions
+ Contact Us
+ DS16
+ Contact Us
+ 2025 Digico Solutions. All Rights Reserved.
+ Subscribe to our Newsletter
+ Subscribe
+ Loading...
+ ×"""
+
+header = """Solutions
+ Cloud Consulting & Migration
+ Assessment & Roadmap
+ Lift & Shift
+ Landing Zone
+ Well-Architected Cloud
+ Modernization
+ Serverless Modernization
+ Storage Modernization
+ Database Modernization
+ DevOps
+ Managed Services
+ Technology Success
+ Security
+ Disaster Recovery
+ FinOps
+ Artificial Intelligence
+ AI Adoption Roadmap
+ AI Solutions
+ AI Applications
+ MLOps
+ Data & Analytics
+ Data Roadmap
+ Data Insights & BI
+ Data Architectures & Pipelines
+ Startup Accelerator
+ Startup Consulting
+ Startup Build Program
+ Cloud Credit & Resources
+ Innovation Hub
+ AI Readiness Assessment
+ Data Readiness Assessment
+ Cloud Readiness Assessment
+ AI Use Cases
+ AI Accelerators
+ AI Hub
+ Case Studies
+ Blog
+ Company
+ About us
+ Who We Are
+ Our Core Values
+ Our Team
+ Sustainability & CSR
+ Careers
+ Our Environment
+ Open Positions
+ Events
+ English
+ X
+ Contact Us
+ Solutions
+ Cloud Consulting & Migration
+ Assessment & Roadmap
+ Lift & Shift
+ Landing Zone
+ Well-Architected Cloud
+ Modernization
+ Serverless Modernization
+ Storage Modernization
+ Database Modernization
+ DevOps
+ Managed Services
+ Technology Success
+ Security
+ Disaster Recovery
+ FinOps
+ Artificial Intelligence
+ AI Adoption Roadmap
+ AI Solutions
+ AI Applications
+ MLOps
+ Data & Analytics
+ Data Roadmap
+ Data Insights & BI
+ Data Architectures & Pipelines
+ Startup Accelerator
+ Startup Consulting
+ Startup Build Program
+ Cloud Credit & Resources
+ Innovation Hub
+ AI Hub
+ AI Readiness Assessment
+ Data Readiness Assessment
+ Cloud Readiness Assessment
+ AI Use Cases
+ AI Accelerators
+ Case Studies
+ Blog
+ Company
+ About us
+ Who We Are
+ Our Core Values
+ Our Team
+ Sustainability & CSR
+ Careers
+ Our Environment
+ Open Positions
+ Events
+ Contact Us
+ English
+ X"""
+
+language = """العربية
+ (
+ Arabic
+ )"""
+
+files_with_newline = [
+    "ai-for-next-generation-growth.txt",
+    "aws-cloud-day-uae.txt",
+    "aws-community-day-beirut-2023.txt",
+    "bedu-streamlined-operations.txt",
+    "driving-customer-growth-through-machine-learning-ai.txt",
+    "leap-2024.txt",
+    "marketing-manager.txt",
+    "senior-aws-fullstack-developer.txt",
+    "smartrise-a-strategic-migration-for-growth.txt",
+    "virtual-minds-revolutionizing-the-use-of-ai.txt"
+]
+
+files_to_skip = [
+    "terms-conditions.txt",
+    "use-cases.txt"
+]
+
+remove_files = [
+    "sa.txt",
+    "thank-you.txt",
+    "404-error-page.txt",
+    "people-culture.txt",
+    "news-reads.txt"
+
+]
+
+remove_phrases = [
+        language,
+        header,
+        footer,
+        "Back to Open Positions",
+        "Read More",
+        "More Details",
+        "Back to Homepage",
+        "☁️",
+        "All Events",
+        "Upcoming Events",
+        "Past Events",
+        " Learn\n more",
+        "More Blogs"
+
+    ]
